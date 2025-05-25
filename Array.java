8ebc86ae1d82499694afffa48b15bd1bbc89ba69
Soumya Priyadarshini <soumyapriyadarshini41@gmail.com>
2025-05-25 22:41:32 +0530

    ArrayList Basic Operations

--- Array.java
+++ Array.java
@@ -141,7 +141,44 @@
 
         
 
-  
+
+//Basic operations on ArrayList
+
+
+import java.util.ArrayList;
+
+public class Array{
+  public static void main(String[] args){
+    ArrayList<Integer> arr = new ArrayList<>(6);// Initializing ArrayList with initial capacity of 6
+    arr.add(0,10);
+    arr.add(1,20);
+    arr.add(2,30);  
+    arr.add(3,40);
+    arr.add(4,50);
+    arr.add(5,60);
+
+    //Arraylist does not have default value like arrays(i.e. 0)
+    //it can grow dynamically
+    //so if we do not add any element at a specific index in between, it will throw an error
+    System.out.println("ArrayList: " + arr);
+   for (int i=0 ;i<arr.size();i++){
+    System.out.print(arr.get(i) + " "); 
+   }
+
+   arr.set(2,300); // Modifying the value at index 2 to 300
+    System.out.println("\nUpdated ArrayList after setting index 2 to 300: " + arr);
+
+
+    arr.add(70); //push back operation
+    //adding an element at the end of the ArrayList
+    System.out.println("ArrayList after adding 70: " + arr);
+    System.out.println("Size of ArrayList: " + arr.size());
+
+    arr.remove(3); // Removing the element at index 3
+    System.out.println("ArrayList after removing element at index 3: " + arr);
+    System.out.println("Size of ArrayList after removal: " + arr.size());
+  }
+}
 
 
 
